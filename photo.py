857bc99a3122865c61373497739fc8774d93f1f2
JustinC <Justincheng12345@gmail.com>
2013-08-11 00:22:23 +0800

    Fix Unicode Bug, Improvements
    The bug is caused by carzy variable.
    Improvements:
    Use more built in method,
    Simplify codes.

--- photo.py
+++ photo.py
@@ -18,7 +18,7 @@ requirnment of fair use.
 
 import os,re,urllib
 import pywikibot
-import simplejson,Myopener
+import Myopener
 
 class FileResizeBot:
     def __init__(self,local):
@@ -29,50 +29,38 @@ class FileResizeBot:
         # as data.api doesn't provide action = 'parse'
 
     def run(self):
-        self.pagename = pywikibot.input('Which page to work on?')
-        if (self.pagename.split('.')[-1]) == 'svg':
-            pywikibot.output('Svg found:'+self.pagename)
+        self.imagename = pywikibot.input('Which page to work on?')
+        self.nofileimagename = self.imagename.split(':')[-1]
+        if (self.imagename.split('.')[-1]) == 'svg':
+            pywikibot.output('Svg found:'+self.imagename)
             #svg should never be re-sized
             return
-        self.do(self.pagename)
+        self.do(self.imagename)
 
     def do(self,page):
         usepage = self.filelink(page)
         if usepage == None:
             return
-        pagename = pywikibot.Page(usepage)
-        lbltitle = pagename.title(asUrl=True)
-        url,size = self.parsetext(lbltitle)
-        self.download(url,size,lbltitle)
+        url,size = self.parsetext(usepage)
+        self.download(url,size)
+        raise ValueError('test')
         if not self.local:
             self.upload()
         # TODO: log for successful upload.
 
-    def parsetext(self,lbltitle):
-        results = self.doitmyself(1,lbltitle)
-        text2 = '//upload.wikimedia.org/wikipedia/zh/thumb/'
-        # TODO: zh -> user-config.mylang
-        title = self.pagename.split(':')[-1]
-        text = title+'" width="'
-        regext = ('('+re.escape(text2)+'\\w{1}/\\w{2}/'+re.escape(title)+
-                  ')/\d*?px-'+re.escape(text)+'(\d*?)\\"')
+    def parsetext(self,usepage):
+        results = pywikibot.data.api.Request(action="parse",
+                                             page=usepage.title()
+                                             ).submit()["parse"]["text"]["*"]
+        pywikibot.output(self.site.lang)
+        imagename = urllib.quote(self.nofileimagename.encode('utf8'))
+        text = imagename+'" width="'
+        regext = ('('+re.escape('//upload.wikimedia.org/wikipedia/'+
+                                self.site.lang+'/thumb/')+'\\w{1}/\\w{2}/'+
+                  re.escape(imagename)+')/\d*?px-'+re.escape(text)+'(\d*?)\\"')
         match=re.search(regext,results, flags=0)
-        if match:
-            return match.group(1),match.group(2)
-        else:
-            ## FIXME:
-            # I don't remember why I should put this here, but this help catch
-            # more results. However, unicode-encoded words are still not catched
-            # and I don't know why....
-            pywikibot.output('a')
-            text = lbltitle+'.'+self.pagename.split('.')[-1]+'" width="'
-            regext = ('('+re.escape(text2)+'\\w{1}/\\w{2}/'+re.escape(lbltitle)+
-                      ')/\d*?px-'+re.escape(text)+'(\d*?)\\"')
-            #FIXME Cannot do with others
-            pywikibot.output(regext)
-            match=re.search(regext,results, flags=0)
-            if match:
-                return match.group(1),match.group(2)
+        return match.group(1),match.group(2)
+        # Return directly as there will still be error if cannot catch
 
     def filelink(self,page):
         fileLinksPage = pywikibot.ImagePage(self.site,page)
@@ -85,35 +73,26 @@ class FileResizeBot:
             return page
         # A fair use image should not be use multiple times.
 
-    def doitmyself(self,time,lbltitle):
-        if time == 1:
-            url = ('https://zh.wikipedia.org/w/api.php?action=parse&format=jso'+
-                   'n&page='+lbltitle)
-        else:
-            url = ('https://zh.wikipedia.org/w/api.php?action=query&prop=image'+
-                   'info&format=json&iiprop=size&titles='+self.pagename)
-        pywikibot.output(url)
-        result = self.opener.open(url)
-        json = simplejson.loads(result.read())
-        if time == 1:
-            return json["parse"]["text"]["*"]
-        else:
-            result = json["query"]["pages"]["436757"]["imageinfo"][0]
-        result = str(result)
-        result = re.search("width': (\\d*?),",result,flags=0).group(1)
-        return result
-        
-
-    def download(self,url,size,lbltitle):
+    def download(self,url,size):
         size = int(size)
         if size+50 < 300:
             size = 300
         else:
             size = size+50
-        if (int(self.doitmyself(0,lbltitle)) <= size):
+        r = (pywikibot.data.api.Request(action='query',prop='imageinfo',
+                                      iiprop='size',titles=self.imagename)
+           .submit())
+        key = str(r["query"]["pages"].keys()).split("[u'")[1].split("']")[0]
+        key = str(r["query"]["pages"].get(key))
+        size2 = int(re.search("width': (\\d*?),",key,flags=0).group(1))
+        # A bit hacky here, but I can't find a way round.
+        pywikibot.output(size)
+        pywikibot.output(size2)
+        if (size2 <= size):
             raise ValueError("Orginal too small")
-        url = 'https:'+url+'/'+str(size)+'px-'+self.pagename.split(':')[-1]
-        self.localf = rewrite_path+'\\Cache\\'+self.pagename.split(':')[-1]
+        url = ('https:'+url+'/'+str(size)+'px-'+
+               urllib.quote(self.imagename.split(':')[-1].encode('utf8')))
+        self.localf = rewrite_path+'\\Cache\\'+self.imagename.split(':')[-1]
         # This should work with using pwb.py or else state it yourself
         self.opener.retrieve(url,self.localf)
 
@@ -137,6 +116,7 @@ class FileResizeBot:
 
 def main():
     local = False
+    cat = False
     bot = FileResizeBot(local)
     bot.run()
 
